e849afcd418a3dd17679283ed2d124ed532995ff
rupesh7070 <rupesh7070@gmail.com>
2020-03-14 18:22:36 +0530

    Delete SearchAcrossArray.java

--- src/main/java/com/anurup/samplePrograms/arrays/SearchAcrossArray.java
+++
@@ -1,8 +0,0 @@
-package com.anurup.samplePrograms.arrays;
-//Question bank page 57
-// Write a program to accept  the name of 10 cities in a single dimension string array and thier STD code  in aother
-//single dimeniosn integer array. Search for a name of city input by user in list field.
-//if found display  "Search Succesfull" and print the name of city along with STD code or else
-//display message " Search Unsuccesful : No such city in list
-public class SearchAcrossArray {
-}
